0f305164a579cf3ebdb7359979cd576f5631c598
Leonel Castillo <leonelcastillo1014@gmail.com>
2022-03-09 22:08:41 -0600

    Primer commit, agregado test de Repository y User

---
+++ database/factories/RepositoryFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Repository;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RepositoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::Factory()
+        ];
+    }
+}
