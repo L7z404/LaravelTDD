2574f78d1c7fa86aaa314ab568dc19486790d163
Leonel Castillo <leonelcastillo1014@gmail.com>
2022-03-12 19:48:36 -0600

    Acomodo final, quedan unos detallitos pero está listo

--- resources/views/repositories/index.blade.php
+++ resources/views/repositories/index.blade.php
@@ -8,11 +8,18 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <p class="text-right mb-4">
+                    <a class="bg-purple-600 text-white font-bold py-2 px-4 rounded-md text-xs hover:bg-purple-800 cursor-pointer" href="{{route('repositories.create')}}">
+                        Agregar un nuevo repositorio
+                    </a>
+                </p>
                 <table>
                     <thead>
                         <tr>
                             <th>ID</th>
                             <th>Enlace</th>
+                            <th>&nbsp;</th>
+                            <th>&nbsp;</th>
                             <th>&nbsp;</th>
                             <th>&nbsp;</th>
                             <th>&nbsp;</th>
@@ -25,20 +32,29 @@
                             <td class="border px-4 py-2">{{$repository->url}}</td>
                             <td>&ensp;</td>
                             <td>
-                                <a class="bg-green-500 text-white font-bold py-2 px-4 rounded-md hover:bg-green-800 cursor-pointer" href="{{route('repositories.show', $repository)}}">
+                                <a class="bg-green-600 text-white font-bold py-2 px-4 rounded-md hover:bg-green-800" href="{{route('repositories.show', $repository)}}">
                                     Ver
                                 </a>
                             </td>
                             <td>&ensp;</td>
                             <td>
-                                <a class="bg-orange-500 text-white font-bold py-2 px-4 rounded-md hover:bg-orange-800 cursor-pointer" href="{{route('repositories.edit', $repository)}}">
+                                <a class="bg-orange-500 text-white font-bold py-2 px-4 rounded-md hover:bg-orange-800" href="{{route('repositories.edit', $repository)}}">
                                     Editar
                                 </a>
+                            </td>
+                            <td>&ensp;</td>
+                            <td>
+                                <form action="{{route('repositories.destroy', $repository)}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    
+                                    <input type="submit" value="Eliminar" class="bg-red-600 text-white font-bold py-2 px-4 rounded-md hover:bg-red-800 cursor-pointer">
+                                </form>
                             </td>
                         </tr>
                         @empty
                         <tr>
-                            <td colspan="6">No hay repositorios creados</td>
+                            <td colspan="8">No hay repositorios creados</td>
                         </tr>
                         @endforelse
                     </tbody>
